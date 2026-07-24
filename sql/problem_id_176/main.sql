25ee6fa08bc918a4857b58521c66e025b9833e98
Lucca Labattaglia <lucca.labattaglia@gmail.com>
2026-07-24 14:47:07 -0600

    did problem 176

---
+++ sql/problem_id_176/main.sql
@@ -0,0 +1,3 @@
+-- Write your PostgreSQL query statement below
+
+SELECT (SELECT salary FROM Employee GROUP BY salary HAVING COUNT (DISTINCT salary) = 1 ORDER BY salary DESC LIMIT 1 OFFSET 1) AS SecondHighestSalary;
